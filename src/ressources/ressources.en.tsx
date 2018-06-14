3ebf2af117e86105f29d6428ae780e4b9b3a75fd
Sevket <sevket.yalcin@outlook.com>
2018-06-15 07:14:14 +0900

    English trads

--- src/ressources/ressources.en.tsx
+++ src/ressources/ressources.en.tsx
@@ -9,24 +9,24 @@ const ressources = {
     introTitle:  'About me',
     introBody: `Enthusiastic developer from France, I am motivated by challenging projects and passionate about Web technologies (especially React and its ecosystem), Test-Driven Development and Agile Development. <br /><br />
     I am currently working at <a target="blank" href="https://www.forcia.com/">Forcia</a> in Tokyo where I focus on building web applications, implementing testing processes and tooling around Application Lifecycle Management. <br /><br />
-    My second passion is Language/Culture Exchange (English and Japanese), it's the reason why I am currently living in Japan!`,
-    skillsTitle:  'Compétences',
-    studiesTitle:  'Études',
-    studiesSchoolBody: `L’exia.CESI forme des ingénieurs professionnels de l’informatique ancrés dans le monde de l’entreprise.<br/> <br/>
-    La pédagogie de l'exia.Cesi est basé sur la méthode d’apprentissage PBL (Problem-Based Learning). Cette pédagogie active place les étudiants dans des situations réelles d’entreprise.`,
-    studiesMoreInfo:  'Plus d\'infos sur le site internet de l\'exia.CESI',
-    studiesMajor:  'Exia, dominante software development',
-    studiesBody: `Apprentissage d'un bon nombre de languages de programmation, de base de données, de techniques de conception de systèmes accompagné de projets d'équipe pour une mise en situation d'entreprise.<br/> <br/>
-    La 5ème année était plus axée vers le management de projet (méthodologies, outils) tout en menant un projet fil rouge (concours Nokia), un projet d'entrepreunariat (site de jeu en ligne), ainsi que la préparation de mon mémoire et de sa présentation (l'implantation de technologie NFC chez l'humain).`,
-    xpTitle:  'Expérience professionnelle',
+    My main hobby is Language and Culture Exchange (English and Japanese), it's the reason why I am currently living in Japan!`,
+    skillsTitle:  'Skills',
+    studiesTitle:  'Studies',
+    studiesSchoolBody: `Exia.Cesi is an engineering school located in many cities in France, specializing in Information Technologies.<br/> <br/>
+    The learning system used at Exia.Cesi is called PBL: Problem-Based Learning. This pedagogy makes students solve complex problems you could have in real company projects.`,
+    studiesMoreInfo:  'More information on Exia.Cesi\'s website (French).',
+    studiesMajor:  'Exia, Software Engineering Master\'s Degree',
+    studiesBody: `Learned how to code using many programming languages, modeling languages and database systems through many team projects during 5 years.<br/> <br/>
+    The fifth and last year is focused on IT projects management, while working on long term projects : Windows Phone App contest by Nokia (as a team), entrepreneurial project (HTML5 Games website as a team), and finally working on my Master Thesis: NFC implants, possibilities and risks.`,
+    xpTitle:  'Work experience',
     xpForcia:  'Forcia',
     xpIS:  'IS',
     xpStage:  'Stage',
     contactTitle:  'Contact',
-    contactName:  'Nom',
-    contactSubjet:  'Objet',
+    contactName:  'Name',
+    contactSubjet:  'Subject',
     contactMessage:  'Message',
-    contactSend:  'Contacter'
+    contactSend:  'Send'
 };
 
 export default ressources;
